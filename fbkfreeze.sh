5331151420fad7ccd105b0a6ba533c96e91cdc62
Hugo Fernando Benitez Kleinert <53825401+fernandobk@users.noreply.github.com>
2023-05-08 21:23:31 -0300

    ...

--- fbkfreeze.sh
+++ fbkfreeze.sh
@@ -1,44 +1,60 @@
 #!/bin/bash
+hoy = $(date +%Y-%m-%d)
+read username < username.conf
+read today < today.conf
+read status < status.conf
 
 case $1 in
 	"-a")
-		echo "Congelado" > status
+		echo "Congelado" > status.conf
 		;;
 	"-d")
-		echo "Descongelado" > status
+		echo "Descongelado" > status.conf
+		;;
+	"-u")
+		echo "Ingrese el nombre del usuario a operar:\n"
+		read _u
+		echo $_u > username.conf
+		echo "Se guardó el nombre de usuario:" $_u
 		;;
 	"-e")
 		empaquetar
 		;;
 	"-v")
-		read estado < status
-		echo $estado
+		echo status
 		;;
 	*)
 		echo "fbkfreeze - Sistema de congelado de carpeta de usuario para debian"
-		echo "";
-		echo "Opciones: ";
-		echo "	-a	Activa el sistema.";
-		echo "	-d	Desactiva el sistema.";
-		echo "	-e	Crear o recrea un paquete de la carpeta /home.";
-		echo "	-r	Restablecer el sistema (Usar con Cron)."
+		echo ""
+		echo "Opciones: "
+		echo "	-a	Activa el sistema."
+		echo "	-d	Desactiva el sistema."
 		echo "	-u	Establecer nombre de usuario."
-		echo "	-v	Ver el estado actual del equipo.";
+		echo "	-v	Ver el estado actual del equipo."
+		echo "	-e	Crear o recrea un paquete de la carpeta /home."
+		echo "	-r	Restablecer el sistema (Usar con Cron)."
 esac
 
 function empaquetar(){
-	read usuario < username
-	if [$usuario == ""];then
+	
+	if [$username == ""];then
 		echo "Falta nombre de usuario:"
 		ls /home
-		echo "Establecer con -u nombreusuario"
+		echo "Establecer con -u"
 		return
 	
 	else
-		tar -v -c -f "/home/fbkfreeze.tar" -C /home $usuario
+		echo "Nombre de usuario registrado:" $username
+		read -p "Presiona Enter para continuar..."
+
+		tar -v -c -f "/home/$usuario.tar" -C /home $usuario
 	fi
 }
 
 function restablecer(){
-	tar -v -x -f "/home/fbkfreeze.tar" -C /home
+	
+	if ["$today" != "$hoy" && $status=="Congelado"]; then
+		rm -R -f -v /home/$username
+		tar -v -x -f "/home/$username.tar" -C /home
+	fi
 }
